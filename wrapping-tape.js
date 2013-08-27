a212eb7791503c728a65de1fcdd438caacd28838
Dale Harvey <dale@arandomurl.com>
2013-08-27 01:13:22 +0100

    Fix function definition

--- wrapping-tape.js
+++ wrapping-tape.js
@@ -2,22 +2,24 @@
 
 var tape = require('tape');
 
-module.exports = return function(opts) {
+module.exports = function(opts) {
+  return function(name, fun) {
 
-  if (opts.setup) {
-    tape('setup', function(t) {
-      t.plan(0);
-      opts.setup(t.end.bind(this));
-    });
-  }
+    if (opts.setup) {
+      tape('setup', function(t) {
+        t.plan(0);
+        opts.setup(t.end.bind(this));
+      });
+    }
 
-  tape(name, fun);
+    tape(name, fun);
 
-  if (opts.teardown) {
-    tape('teardown', function(t) {
-      t.plan(0);
-      opts.teardown(t.end.bind(this));
-    });
-  }
+    if (opts.teardown) {
+      tape('teardown', function(t) {
+        t.plan(0);
+        opts.teardown(t.end.bind(this));
+      });
+    }
 
+  };
 };
